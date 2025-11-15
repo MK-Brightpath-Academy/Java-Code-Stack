19f0b3844a1fb1f7bfa2cb20d79f018d1aaf3224
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2025-11-15 22:53:44 +0530

    Time: 4 ms (82.54%) | Memory: 96.8 MB (6.36%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+         if (head == null || head.next == null) return true;
+
+        // Step 1: Find middle using slow & fast pointers
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode secondHalfHead = reverseList(slow);
+
+        // Step 3: Compare both halves
+        ListNode p1 = head;
+        ListNode p2 = secondHalfHead;
+        boolean result = true;
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                result = false;
+                break;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        reverseList(secondHalfHead);
+
+        return result;
+    }
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
